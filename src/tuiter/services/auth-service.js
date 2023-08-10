06890445b28e4bd3f35c4cd45357f92c8dcbdbd5
natha <capture12020@gmail.com>
2023-08-10 17:31:51 -0400

    updating API base variables

--- src/tuiter/services/auth-service.js
+++ src/tuiter/services/auth-service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const SERVER_API_URL = 'http://localhost:4000/api';
-const SERVER_API_URL = process.env.REACT_APP_SERVER_API_URL;
+const SERVER_API_URL = process.env.REACT_APP_API_BASE;
 const USERS_URL = `${SERVER_API_URL}/users`; // URL to auth controller
 
 // configure axios to support cookies for passing credentials
